cca1e3115c743559a6ef526158ca7135d3f6b9b5
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-10-02 23:39:29 +0900

    filtering width < height rectangle

--- rect_edit/main.py
+++ rect_edit/main.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from email.mime import image
 from email.policy import strict
 from pathlib import Path
+from typing import Any
 from pdf2image import convert_from_path
 import cv2 as cv
+import numpy as np
 
 
 @dataclass(frozen=True)
@@ -20,7 +23,6 @@ def convert_pdf2jpeg(src_pdf_path: str, export_dir: str) -> None:
     for i, page in enumerate(pages):
         file_name = pdf_path.stem + "_{:02d}".format(i + 1) + ".jpeg"
         image_path = image_dir / file_name
-        # JPEGで保存
         page.save(str(image_path), "JPEG")
 
 
@@ -31,38 +33,43 @@ def find_rectangles(image_path: str) -> list[Rectangle]:
 
     img_gray = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
 
-    _, dst = cv.threshold(img_gray, 127, 255, cv.THRESH_TOZERO_INV)
-
+    _, dst = cv.threshold(img_gray, 230, 255, cv.THRESH_BINARY)
     dst = cv.bitwise_not(dst)
+    
+    kernel = np.ones((3,3),np.uint8)
+    dst = cv.dilate(dst,kernel,iterations = 1)
 
     _, dst = cv.threshold(dst, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
 
     contours, _ = cv.findContours(dst, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    dst = cv.imread(image_path, cv.IMREAD_COLOR)
-    dst = cv.drawContours(dst, contours, -1, (0, 0, 255, 255), 2, cv.LINE_AA)
-    cv.imwrite("debug_1.jpeg", dst)
-    dst = cv.imread(image_path, cv.IMREAD_COLOR)
+    rects = __filter(image_size, contours)
+    return rects
 
+def __filter(image_size : int, contours: Any)->list[Rectangle]:
     rects: list[Rectangle] = []
-    for i, contour in enumerate(contours):
-        # 小さな領域の場合は間引く
+    for _, contour in enumerate(contours):
         area = cv.contourArea(contour)
-        if area < 500:
+        if area < 1000:
             continue
-        # 画像全体を占める領域は除外する
+
         if image_size * 0.99 < area:
             continue
 
-        # 外接矩形を取得
         x, y, w, h = cv.boundingRect(contour)
+
+        if w < h:
+            continue
+
         rect = Rectangle(x, y, w, h)
         rects.append(rect)
+    return rects
 
 
 convert_pdf2jpeg("./sample.pdf", "./image")
 rects = find_rectangles("./image/sample_01.jpeg")
 dst = cv.imread("./image/sample_01.jpeg", cv.IMREAD_COLOR)
+rect_color = (0, 255, 0)
 for r in rects:
-    dst = cv.rectangle(dst, (r.x, r.y), (r.x + r.w, r.y + r.h), (0, 255, 0), 2)
+    dst = cv.rectangle(dst, (r.x, r.y), (r.x + r.w, r.y + r.h), rect_color, 2)
 cv.imwrite("result.jpeg", dst)
